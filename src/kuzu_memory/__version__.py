8cca32e1c6e77d3a792da41aba4cc2e07044fd81
Robert (Masa) Matsuoka <bobmatnyc@users.noreply.github.com>
2025-10-02 10:28:04 -0400

    chore: update version files to 1.1.12 for PyPI release

--- src/kuzu_memory/__version__.py
+++ src/kuzu_memory/__version__.py
@@ -1,6 +1,6 @@
 """Version information for KuzuMemory."""
 
-__version__ = "1.1.11"
+__version__ = "1.1.12"
 __version_info__ = tuple(int(i) for i in __version__.split("."))
 
 # Database schema version for migration support
